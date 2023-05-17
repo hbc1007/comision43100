c2cc7d75617410746c9f25f0674c84ee279272e7
humberto balcazar <hbc1007@gmail.com>
2023-05-16 23:45:08 -0500

    carga inicial proyecto

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// ------------------------------------------------------------------//
+const div = document.querySelector('header > div');
+
+window.addEventListener('scroll', function() {
+    div.classList.toggle('abajo', window.scrollY > 0);
+})
+
+// ------------------------------------------------------------------//
