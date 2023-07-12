5b2d908a2d74005a5877f59ac275d1e25699c7ce
humberto balcazar <hbc1007@gmail.com>
2023-07-11 21:54:10 -0500

    modificacion logica del carrito de compras

--- js/logica.js
+++ js/logica.js
@@ -1,8 +1,50 @@
 //---------------------------------------------------------------------------------------//
+//CLASE - PRODUCTOS EN CARRITO
+//---------------------------------------------------------------------------------------//
+class clsProducto {
+    constructor(obj) {
+        this.quantity = parseFloat(obj.quantity),
+        this.name = obj.name,
+        this.price = parseFloat(obj.price),
+        this.id = obj.id
+    }
+}
+
+
+let todosProductos = [];
+let cantCarrito = 0;
+let contenedorProductos = document.getElementById('listaProductos');
+let cartaInfo = document.querySelector('.cart-product');
+let filaProductos = document.querySelector('.row-product');
+let listaProductos = document.querySelector('.container-items');
+let listaBolsa = document.querySelector('.container-cart-icon');
+let valorTotal = document.querySelector('.total-pagar');
+let contProduct = document.querySelector('#count-products');
+let cartEmpty = document.querySelector('.cart-empty');
+let cartTotal = document.querySelector('.cart-total');
+
+
+//---------------------------------------------------------------------------------------//
+//FUNCION - CANTIDAD TOTAL EN BOLSA DEL CARRITO DE COMPRAS
+//---------------------------------------------------------------------------------------//
+function cantidadBolsa(){
+    let productos = [];
+    const almacenados = JSON.parse(localStorage.getItem('todos-Productos'));
+    if (almacenados != null){
+        for (const objeto of almacenados)
+        //INSERTA EN LA CLASE clsProductos TODO HAY EN EL LOCALSTORAGE
+        productos.push(new clsProducto(objeto));
+    
+        //SUMA LA CANTIDAD DE PRODUCTOS PARA QUE SE MUESTRE EN LA BOLSA
+        for (const producto of productos){
+            cantCarrito += producto.quantity;
+        }
+    }
+}
+
+//---------------------------------------------------------------------------------------//
 //FUNCION - RENDERIZACION DE PRODUCTOS
 //---------------------------------------------------------------------------------------//
-let contenedorProductos = document.getElementById('listaProductos');
-
 function renderizarProductos(objProductos){
     //VACIAMOS EL CONTENEDOR PARA EVITAR DUPLICADOS
     contenedorProductos.innerHTML='';
@@ -21,22 +63,24 @@ function renderizarProductos(objProductos){
         </div>
         `;
     }
+}
 
-    //CAMBIA EL COLOR DEL BOTON AL MOMENTO DE POSICIONARSE
+
+//---------------------------------------------------------------------------------------//
+//FUNCION - CAMBIA EL COLOR DEL BOTON AL MOMENTO DE POSICIONARSE
+//---------------------------------------------------------------------------------------//
+function cambiarColorBotones () {
     let botones = document.getElementsByClassName('btn-add-cart');
     for (const boton of botones){
         boton.onmouseover = () => {
             boton.classList.replace('botonCompra','botonColor');
-            }
-    
-            boton.onmouseout = () => {
-                boton.classList.replace('botonColor','botonCompra');
-            }
+        }
+        boton.onmouseout = () => {
+            boton.classList.replace('botonColor','botonCompra');
+        }
     }
 }
 
-console.log(productos);
-renderizarProductos(productos);
 
 //---------------------------------------------------------------------------------------//
 //FUNCION - FORMATO NUMEROS
@@ -47,52 +91,72 @@ function formatearValor(valor, decimal){
         currency: "PEN",
         maximumFractionDigits: decimal,
     });
-
     return formateador.format(valor);
 };
 
+
+
+
 //---------------------------------------------------------------------------------------//
-//MUESTRA U OCULTA AL DAR CLICK EN LA VENTANA RESUMEN DEL CARRITO DE COMPRAS
+//---------------------------------------------------------------------------------------//
+//---------------------------------------------------------------------------------------//
+//---------------------------------------------------------------------------------------//
+renderizarProductos(productos);
+cambiarColorBotones();
+cantidadBolsa();
+contProduct.innerText = cantCarrito;
+
+
+//---------------------------------------------------------------------------------------//
+//FUNCION FLECHA - MUESTRA U OCULTA VENTANA DEL CARRITO DE COMPRAS AL DAR CLICK EN LA BOLSA
 //---------------------------------------------------------------------------------------//
 const botonBolsaCompras = document.querySelector('.container-cart-icon');
 const contenedorCartaProductos = document.querySelector('.container-cart-products');
 
 botonBolsaCompras.addEventListener('click', () => {
     contenedorCartaProductos.classList.toggle('hidden-cart');
+    vistaHTML();
 });
 
+
 //---------------------------------------------------------------------------------------//
-//VARIABLES - QUERY SELECTOR
+//FUNCION FLECHA - CARGA INCIAL DEL CARRITO DE COMPRAS
 //---------------------------------------------------------------------------------------//
-let todosProductos = [];
-const cartaInfo = document.querySelector('.cart-product');
-const filaProductos = document.querySelector('.row-product');
-const listaProductos = document.querySelector('.container-items');
-const valorTotal = document.querySelector('.total-pagar');
-const contProduct = document.querySelector('#count-products');
-const cartEmpty = document.querySelector('.cart-empty');
-const cartTotal = document.querySelector('.cart-total');
+// listaBolsa.addEventListener('click', e => {
+//     if (e.target.classList.contains('icon-cart')){
+//         console.log("click en la bolsa del carrito")
+//         vistaHTML();
+//     }
+// })
 
 
+//---------------------------------------------------------------------------------------//
+//FUNCION FECHA - INSERTA LA LINEA DEL PRODUCTO AL CARRITO DE COMPRAS
 //---------------------------------------------------------------------------------------//
 listaProductos.addEventListener('click', e => {
     if (e.target.classList.contains('btn-add-cart')){
+        let recoveredData = localStorage.getItem('todos-Productos')
+        if (recoveredData != null){
+            todosProductos = JSON.parse(recoveredData);
+        }
+
         const product = e.target.parentElement;
 
         const infoProducto = {
             quantity: 1,
             name: product.querySelector('.name').textContent,
             price: product.querySelector('.price').textContent,
+            id: product.querySelector('button').getAttribute('id'),
         };
 
-        console.log(infoProducto);
-
         //METODO SOME, BUSCA EL PRIMER ELEMENTO ENCONTRADO Y DEVUELVE TRUE O FALSE
-        const exists = todosProductos.some(product => product.name === infoProducto.name)
+        const exists = todosProductos.some(product => product.id === infoProducto.id)
 
+        //
         if (exists){
+            //ACTUALIZACION DE LA CANTIDAD POR PRODUCTO
             const products = todosProductos.map(product => {
-                if(product.name === infoProducto.name){
+                if(product.id === infoProducto.id){
                     product.quantity++; //SUMA
                     return product;
                 }
@@ -110,35 +174,34 @@ listaProductos.addEventListener('click', e => {
 
         //AGREGA DATOS DEL CARRITO DE COMPRAS EN EL LOCAL STORAGE
         localStorage.setItem("todos-Productos", JSON.stringify(todosProductos));
-        //console.log(todosProductos);
         vistaHTML();
     }
 })
 
+
 //---------------------------------------------------------------------------------------//
-//FUNCION - INSERTA Y MUESTRA LA LINEA DEL PRODUCTO AGREGADO AL CARRITO DE COMPRAS
+//FUNCION FECHA - ACTUALIZA LA LINEA DEL PRODUCTO AGREGADO AL CARRITO DE COMPRAS
 //---------------------------------------------------------------------------------------//
 filaProductos.addEventListener('click', e => {
     if (e.target.classList.contains('icon-close')){
         const product = e.target.parentElement;
-        const name = product.querySelector('.titl-prod-cart').textContent;
-        todosProductos = todosProductos.filter(product => product.name !== name);
-
-        //AGREGA DATOS DEL CARRITO DE COMPRAS EN EL LOCAL STORAGE
-        localStorage.setItem("todos-Productos", JSON.stringify(todosProductos))
-        //console.log(todosProductos);
+        const idPro = product.querySelector('.codi-prod-cart').textContent;
+         //RECUPERA DATOS DEL LOCAL STORAGE
+        todosProductos = JSON.parse(localStorage.getItem("todos-Productos"));
+        todosProductos = todosProductos.filter(product => product.id !== idPro);
+        //GUARDA EN EL LOCAL STORAGE
+        localStorage.setItem("todos-Productos", JSON.stringify(todosProductos));
         vistaHTML();
     }
 });
 
+
 //---------------------------------------------------------------------------------------//
-//FUNCION - CARGAR Y MOSTRAR LA VENTANA DEL CARRITO DE COMPRAS
+//FUNCION FLECHA - MUESTRA LOS PRODUCTOS EN LA VENTANA DEL CARRITO DE COMPRAS
 //---------------------------------------------------------------------------------------//
 const vistaHTML = () =>{
-    //RECUPERA DATOS DEL LOCAL STORAGE (productosEnCarrito) PARA 
-    //MOSTRARLO EN LA VENTANA RESUMEN DEL CARRITO DE COMPRAS
+    //RECUPERA DATOS DEL LOCAL STORAGE
     const productosEnCarrito = JSON.parse(localStorage.getItem("todos-Productos"));
-    console.log(productosEnCarrito);
 
     if (!productosEnCarrito.length) {
 		cartEmpty.classList.remove('hidden');
@@ -165,6 +228,7 @@ const vistaHTML = () =>{
                 <p class="cant-prod-cart">${product.quantity}</p>
                 <p class="titl-prod-cart">${product.name}</p>
                 <p class="prec-prod-cart">${product.price}</p>
+                <p class="codi-prod-cart">${product.id}</p>
             </div>
             <img class="icon-close" src="../assets/img/Equis.png" alt="Equis">
             `;
@@ -173,7 +237,6 @@ const vistaHTML = () =>{
 
         total = total + parseFloat(product.quantity * product.price.slice(3));
         totalOfProductos = totalOfProductos + product.quantity;
-        
     })
 
     let valorFormat = formatearValor(`${total}`, 2);
